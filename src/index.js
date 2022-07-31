c419ff675a60b9f6cd0c57704f540e7f79cf9c3a
dbataeva <b.daffa.v@yandex.ru>
2022-07-31 15:42:23 +0300

    feat: check works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let result = true;
+
+  for (let char of str) {
+    bracketsConfig.forEach(item => {
+      const index = item.indexOf(char);
+
+      if (index === -1) {
+        return;
+      }
+
+      if (item[0] === item[1]) {
+        checkWhenCharsAreSame(stack, item);
+      } else {
+        result = checkWhenCharsAreDifferent(index, stack, char, item);
+      }
+    });
+    if (!result) {
+      return result;
+    }
+  }
+  return !stack.length;
+}
+
+function checkWhenCharsAreSame(stack, item) {
+  if (stack[stack.length - 1] === item[0]) {
+    stack.pop();
+  } else {
+    stack.push(item[0]);
+  }
+
+  return ;
+}
+
+function checkWhenCharsAreDifferent(index, stack, char, item) {
+  if (index === 0) {
+    stack.push(char);
+  } else if (index === 1) {
+    if (stack.pop() !== item[0]) {
+      return false;
+    }
+  }
+
+  return true;
 }
